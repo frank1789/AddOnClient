0fd8998169ae36371d5a7127d1f61c334a283af4
frank1789 <francesco.argentieri89@gmail.com>
2017-11-02 15:42:31 +0100

    minor fix

--- AddOnManager/__init__.py
+++ AddOnManager/__init__.py
@@ -2,3 +2,4 @@
 from .Maketitle import Maketitle
 from .Addonupdate import Addonupdate
 from .Filemanager import Filemanager
+from .notifyme import notify
--- AddOnManager/notifyme.py
+++ AddOnManager/notifyme.py
@@ -9,8 +9,6 @@ def notify(title, subtitle, message):
         """osascript -e 'display notification "{}" with title "{}" subtitle "{}" sound name "Pop"'""".format(m, t, s))
 
 
-#
-#
 if __name__ == '__main__':
     # Calling the function
     notify(title='A Real Notification', subtitle='with python', message='Hello, this is me, notifying you!')
