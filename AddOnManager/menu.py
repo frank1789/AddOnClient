f5f5776365ced083734b7fb4dd35ce22f592e18c
Francesco <francesco.argentieri89@gmail.com>
2018-07-19 18:22:08 +0200

    add::sparkles:new menù selection

--- AddOnManager/menu.py
+++ AddOnManager/menu.py
@@ -1,8 +1,10 @@
 # !/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import sys
 
-class Maketitle:
+
+class MakeTitle:
     __name = "Francesco"
     __surname = "Argentieri"
     __License = "MIT"
@@ -10,9 +12,9 @@ class Maketitle:
     def __init__(self):
         self.title()
         self.information()
+        self.menu()
 
-    @staticmethod
-    def title():
+    def title(self):
         # print text art ascii title
         print("___________.__        ____ ___.___     _____       .___  .___________\n"
               "\_   _____/|  |___  _|    |   \   |   /  _  \    __| _/__| _/\_____  \   ____ \n"
@@ -30,3 +32,40 @@ class Maketitle:
         print("\tVersion {}".format(0),
               "\t\tAuthor: {!s}".format(self.__name + " " + self.__surname),
               "\n\tLicense: {!s}".format(self.__License), end='\n')
+
+    def install(self):
+        print ("You typed zero.\n")
+
+    def update(self):
+        print("n is a perfect square\n")
+
+    def remove(self):
+        print("n is an even number\n")
+
+
+
+    def menu(self):
+        print("1. Install", end='\n')
+        print("2. Update", end='\n')
+        print("3. Remove", end='\n')
+        print("4. Exit", end='\n')
+        while True:
+            select = input("select: ")
+            if int(select) == 1:
+                 self.install()
+
+            elif int(select) == 2:
+                self.update()
+
+            elif int(select) == 3:
+                self.remove()
+
+            elif int(select) == 4:
+                sys.exit()
+
+            else:
+                print("wrong selection", end='\n')
+
+
+if __name__ == '__main__':
+     MakeTitle()
