415b25212c0a8952a6eab10a7fe84abd962f041a
Francesco <francesco.argentieri89@gmail.com>
2018-08-11 17:23:25 +0200

    add: information package

--- AddOnManager/__init__.py
+++ AddOnManager/__init__.py
@@ -1,6 +1,13 @@
-"""AddOnManager"""
+"""AddOnManager
+simple script to manage ElvUI from tukui.org
+
+Author: Francesco Argentieri
+Website: https://github.com/frank1789
+Last edited: August 2018
+"""
 from .menu import MakeTitle
 from .Addonupdate import Addonupdate
 from .Filemanager import Filemanager
-#from .notifyme import notify
 from .colour import PrintColour
+from .gui import Example
+# from .notifyme import notify
