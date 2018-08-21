c1d880e3f0c446257531add5af1820c115e33233
Francesco <francesco.argentieri89@gmail.com>
2018-08-21 12:10:54 +0200

    bug: incorrect string use

--- AddOnManager/colour.py
+++ AddOnManager/colour.py
@@ -20,17 +20,17 @@ class PrintColour:
     }
 
     __background = {
-        'bg_black': "\e[40m",
-        'bg_red': "\e[41m",
-        'bg_green': "\e[42m",
-        'bg_brown': "\e[43m",
-        'bg_blue': "\e[44m",
-        'bg_magenta': "\e[45m",
-        'bg_cyan': "\e[46m",
-        'bg_gray': "\e[47m",
+        'bg_black': 40,
+        'bg_red': 41,
+        'bg_green': 42,
+        'bg_brown': 43,
+        'bg_blue': 44,
+        'bg_magenta': 45,
+        'bg_cyan': 46,
+        'bg_gray': 47,
     }
 
-    __fontstyle = {
+    __style = {
         'bold': 1,
         'italic': 3,
         'underline': 4,
@@ -39,20 +39,17 @@ class PrintColour:
     }
 
     __reset = {
-        'all': "\e[0m",
-        'bold': "\e[22m",
-        'italic': "\e[23m",
-        'underline': "\e[24m",
-        'blink': "\e[25m",
-        'reverse_color': "\e[27m"
+        'all': 0,
+        'bold': 22,
+        'italic': 23,
+        'underline': 24,
+        'blink': 25,
+        'reverse_color': 27
     }
 
     text = ""
-
     _colour = 30
-
     _background = 0
-
     _fontstyle = 0
 
     def __init__(self, message):
@@ -70,18 +67,32 @@ class PrintColour:
             return self
 
     def setfontstyle(self, attributes):
-        if attributes in self.__fontstyle:
-            self._fontstyle = self.__fontstyle.get(attributes)
+        if attributes in self.__style:
+            self._fontstyle = self.__style.get(attributes)
             return self
 
-    def setcolorandformat(self, colour=0, background=0, attributes=0):
-        print("\e[1;31;47m #{self} #{RESET[all]}")
+    def setcolorandformat(self, colour, background, style=""):
+        if style == "":
+            if colour in self.__colour:
+                self._colour = self.__colour.get(colour)
+
+            if background in self.__background:
+                self._background = self.__background.get(background)
+
         return self
-
+# TODO torvare l'errore per la stampa corretta nella string, la sequenza comandi modificatori è corretta
     def __str__(self):
-        return "\x1b[{!s}m{!s}\x1b[0m".format(self._colour, self.text)
-
+        if self._colour != 30 and self._background == 0:
+            return "\x1b[{!s}m{!s}\x1b[0m".format(self._colour, self.text)
+        elif self._background != 0:
+            return "\x1b[{!s}m{!s}\x1b[0m".format(self._background, self.text)
+        elif self._fontstyle != 0:
+            return "\x1b[{!s}m{!s}\x1b[0m".format(self._fontstyle, self.text)
+        elif self._colour != 30 and self._background != 0:
+            return "\x1b[{!s};{!s};{!s}m{!s}\x1b[0m".format(self._colour, self._fontstyle, self._background, self.text)
 
 
 if __name__ == '__main__':
-    print(PrintColour("hello world").setcolour('red'))
+    print(PrintColour("hello world").setbackground("bg_red"))
+    print(PrintColour("hello world").setcolorandformat("white", "bg_red", ""))
+    print(PrintColour("hello world").setfontstyle("italic"))
